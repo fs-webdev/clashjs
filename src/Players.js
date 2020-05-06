b83d9f0ef429d3bd68b69e19168b6678c04c0d7a
Jakob Anderson <janderson@familysearch.org>
2020-05-06 14:03:13 -0600

    lrbot added

--- src/Players.js
+++ src/Players.js
@@ -2,5 +2,6 @@ module.exports = {
   starterbot: require("./players/starterbot"),
   random: require("./players/randombot"),
   jakob: require("./players/jakobbot"),
-  // beasty: require("./players/beasty"),
+  larissa: require("./players/lrBOT"),
+  beasty: require("./players/beasty"),
 };
--- src/players/lrBOT.js
+++ src/players/lrBOT.js
@@ -6,16 +6,54 @@ import {
   threatsFacingMe,
   canMoveForward,
   enemiesInRange,
+  isActionSafe,
 } from "../lib/helpers";
 
 import debug from "debug";
 const log = debug("clashjs:bot:lrbot");
 
+function getClosestPlayerToAmmo(closestAmmo, player, enemies) {
+  const ourDistanceFromAmmo = calculateDistance(player.position, closestAmmo);
+  let closestPlayerToAmmo = player;
+  let smallestDistanceFromAmmo = ourDistanceFromAmmo;
+
+  enemies.forEach(enemy => {
+    const enemyDistanceFromAmmo = calculateDistance(enemy.position, closestAmmo);
+    if (enemyDistanceFromAmmo < smallestDistanceFromAmmo) {
+      closestPlayerToAmmo = enemy;
+      smallestDistanceFromAmmo = enemyDistanceFromAmmo;
+    }
+  });
+
+  return closestPlayerToAmmo;
+}
+
+function targetClosestPlayerToAmmo(closestAmmo, player) {
+  let headingDir;
+  if (closestAmmo[0] - player.position[0] === 0 || closestAmmo[1] - player.position[1] === 0) {
+    headingDir = calculateHeading(player.position, closestAmmo);
+  } else if (Math.abs(closestAmmo[0] - player.position[0]) <  Math.abs(closestAmmo[1] - player.position[1])) {
+    headingDir = calculateHeading(player.position, [closestAmmo[0], player.position[1]]);
+  } else {
+    headingDir = calculateHeading(player.position, [closestAmmo[1], player.position[0]]);
+  }
+
+  return turnOrMove(headingDir, player);
+}
+
+function turnOrMove(headingDir, player) {
+  if (headingDir === player.direction) {
+    return "move";
+  } else {
+    return headingDir;
+  }
+}
+
 export default {
   info: {
     name: "lrbot",
     style: 20,
-    team: 3,
+    team: 10,
   },
   ai: function (player, enemies, game) {
     log("Executing my AI function", player, enemies, game);
@@ -24,13 +62,13 @@ export default {
     const targets = enemiesInRange(player, enemies);
     if (player.ammo > 0 && targets.length > 0) {
       log("Found someone to shoot", targets);
-      return "shoot";
+      return "shoot"
     }
 
     // Check if we are in immediate danger, if so try to move
     if (threatsFacingMe(player, enemies).length > 0) {
       log("In danger! Lets try to move");
-      if (canMoveForward(player, game)) {
+      if (canMoveForward(player, game) && isActionSafe(player, 'move', enemies, game)) {
         return "move";
       }
     }
@@ -38,60 +76,29 @@ export default {
     // Not in danger, nobody to shoot, lets go collect more ammo
     const closestAmmo = findClosestAmmo(player, game);
 
-    //If another player is closer to the ammo, try to line up our shot to hit them.
-    const ourDistanceFromAmmo = calculateDistance(player.position, closestAmmo);
-    let closestPlayerToAmmo = player;
-    let smallestDistanceFromAmmo = ourDistanceFromAmmo;
-
-    enemies.forEach(enemy => {
-      const enemyDistanceFromAmmo = calculateDistance(enemy.position, closestAmmo);
-      if (enemyDistanceFromAmmo < smallestDistanceFromAmmo) {
-        closestPlayerToAmmo = enemy;
-        smallestDistanceFromAmmo = enemyDistanceFromAmmo;
-      }
-    });
-
-    if (closestPlayerToAmmo !== player) {
-      const headingDir = calculateHeading(player.position, closestAmmo);
-    }
-
-    //Otherwise head for the ammo
-
     if (closestAmmo) {
       log("Found some ammo", closestAmmo);
-      //If another player is closer to the ammo, try to line up our shot to hit them.
-      if (player.ammo > 0) {
-        const ourDistanceFromAmmo = calculateDistance(player.position, closestAmmo);
-        let closestPlayerToAmmo = player;
-        let smallestDistanceFromAmmo = ourDistanceFromAmmo;
+      const closestPlayerToAmmo = getClosestPlayerToAmmo(closestAmmo, player, enemies);
+      if (closestPlayerToAmmo !== player) {
+        return targetClosestPlayerToAmmo(closestAmmo, player);
+      }
 
-        enemies.forEach(enemy => {
-          const enemyDistanceFromAmmo = calculateDistance(enemy.position, closestAmmo);
-          if (enemyDistanceFromAmmo < smallestDistanceFromAmmo) {
-            closestPlayerToAmmo = enemy;
-            smallestDistanceFromAmmo = enemyDistanceFromAmmo;
-          }
-        });
+      const ammoDir = calculateHeading(player.position, closestAmmo);
 
-        if (closestPlayerToAmmo !== player) {
-          let headingDir;
-          if (Math.abs(closestAmmo[0] - player.position[0]) <  Math.abs(closestAmmo[1] - player.position[1])) {
-            headingDir = calculateHeading(player.position, [closestAmmo[0], player.position[1]]);
-          } else {
-            headingDir = calculateHeading(player.position, [closestAmmo[1], player.position[0]]);
-          }
-
-          if (headingDir === player.direction) {
-            return "move";
-          } else {
-            return headingDir;
-          }
-        }
+      log("Heading towards ammo", ammoDir);
+      if (ammoDir === player.direction) {
+        const safe = isActionSafe(player, 'move', enemies, game)
+        if (safe) return "move";
+      } else {
+        return ammoDir;
       }
     }
 
     // Nothing else to do ... lets just make a random move
     log("Bummer, found nothing interesting to do ... making random move");
-    return makeRandomMove();
+    const safe = isActionSafe(player, 'move', enemies, game);
+    if (safe) {
+      return makeRandomMove();
+    }
   },
 };
